897c7f100c675fd6212da910eb7cd403b6e17994
estherjibriin <119973919+estherjibriin@users.noreply.github.com>
2023-09-19 13:56:41 -0600

    Add files via upload

---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Studentinfo from '../studentinfo';
+
+export default function week2page(){
+    return (
+        <div>
+            <h1>My Shopping List</h1>
+            <Studentinfo/>
+        </div>
+    );
+}
